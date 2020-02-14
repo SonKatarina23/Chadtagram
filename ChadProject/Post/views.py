3e4f5757c38e02c54b643f13872516cdb2aafdb5
SonKatarina23 <houwenlie98@gmail.com>
2020-02-14 13:10:18 +0700

    Revise Navbar and Home Page

--- ChadProject/Post/views.py
+++ ChadProject/Post/views.py
@@ -7,6 +7,11 @@ class PostList(ListView) :
   template_name = 'Post/PostList.html'
   context_object_name = 'post_list'
 
+  def get_queryset(self):
+      return Post.objects.all().order_by('-created_at')
+  
+  
+
 class PostDetail(DetailView) :
   model = Post
   template_name = 'Post/PostDetail.html'
